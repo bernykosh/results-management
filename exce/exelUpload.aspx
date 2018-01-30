46ceaa334b650d5f972eb385d982a582a328ce85
Bernedette Perera <35946933+bernykosh@users.noreply.github.com>
2018-01-30 15:40:35 +0530

    Excel Function
    Implementation of file upload function for marks spreadsheet

---
+++ exce/exelUpload.aspx
@@ -0,0 +1,136 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="exelUpload.aspx.cs" Inherits="Final_Assignment.ManageResults.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style3 {
+            text-align: right;
+            width: 177px;
+            height: 41px;
+        }
+        .auto-style4 {
+            width: 177px;
+        }
+        .auto-style5 {
+            color: #00CC00;
+            font-size: x-large;
+        }
+        .auto-style6 {
+            text-align: right;
+            width: 177px;
+            height: 43px;
+        }
+        .auto-style7 {
+            height: 43px;
+        }
+        .auto-style8 {
+            height: 41px;
+        }
+        .auto-style9 {
+            color: #FF3300;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+        <strong>Upload results |
+        <asp:LinkButton ID="lbHome" runat="server" OnClick="lbHome_Click">Home</asp:LinkButton>
+&nbsp;|
+        <asp:LinkButton ID="lbAddResults" runat="server" OnClick="lbAddResults_Click">Add Results</asp:LinkButton>
+&nbsp;|
+        <asp:LinkButton ID="lbLogout" runat="server" OnClick="lbLogout_Click">Logout</asp:LinkButton>
+&nbsp;| <br />
+        <br />
+        </strong>
+    
+    </div>
+        <table class="auto-style2">
+            <tr>
+                <td class="auto-style6">Upload File : </td>
+                <td class="auto-style7">
+                    <asp:FileUpload ID="ufExel" runat="server" BackColor="#0099FF" BorderColor="#999999" Height="21px" Width="219px" />
+                    <asp:Label ID="lblFileStatus" runat="server" Text="Label" Visible="False"></asp:Label>
+                </td>
+                <td class="auto-style7"></td>
+            </tr>
+            <tr>
+                <td class="auto-style3">
+                    <asp:Button ID="btnSave" runat="server" Height="25px" OnClick="btnSave_Click" Text="Save Result" Width="113px" />
+                </td>
+                <td class="auto-style8"></td>
+                <td class="auto-style8"></td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <asp:Label ID="lblStatus" runat="server" CssClass="auto-style5" Text="Data has been saved" Visible="False"></asp:Label>
+        <asp:TextBox ID="unitCode" runat="server" Visible="False"></asp:TextBox>
+        <asp:TextBox ID="studentID" runat="server" Visible="False"></asp:TextBox>
+        <asp:TextBox ID="semester" runat="server" Visible="False"></asp:TextBox>
+        <asp:TextBox ID="year" runat="server" Visible="False"></asp:TextBox>
+        <asp:TextBox ID="a1" runat="server" Visible="False"></asp:TextBox>
+        <asp:TextBox ID="a2" runat="server" Visible="False"></asp:TextBox>
+        <asp:TextBox ID="exam" runat="server" Visible="False"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblError" runat="server" CssClass="auto-style9" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <asp:SqlDataSource ID="dsInsertResult" runat="server" ConflictDetection="CompareAllValues" ConnectionString="<%$ ConnectionStrings:LoginConnectionString %>" DeleteCommand="DELETE FROM [ResultData] WHERE [ID] = @original_ID AND [UnitCode] = @original_UnitCode AND [StudentID] = @original_StudentID AND (([SemesterID] = @original_SemesterID) OR ([SemesterID] IS NULL AND @original_SemesterID IS NULL)) AND (([Year] = @original_Year) OR ([Year] IS NULL AND @original_Year IS NULL)) AND (([Assigment_01] = @original_Assigment_01) OR ([Assigment_01] IS NULL AND @original_Assigment_01 IS NULL)) AND (([Assigment_02] = @original_Assigment_02) OR ([Assigment_02] IS NULL AND @original_Assigment_02 IS NULL)) AND (([Exam] = @original_Exam) OR ([Exam] IS NULL AND @original_Exam IS NULL))" InsertCommand="INSERT INTO [ResultData] ([UnitCode], [StudentID], [SemesterID], [Year], [Assigment_01], [Assigment_02], [Exam]) VALUES (@UnitCode, @StudentID, @SemesterID, @Year, @Assigment_01, @Assigment_02, @Exam)" OldValuesParameterFormatString="original_{0}" SelectCommand="SELECT * FROM [ResultData]" UpdateCommand="UPDATE [ResultData] SET [UnitCode] = @UnitCode, [StudentID] = @StudentID, [SemesterID] = @SemesterID, [Year] = @Year, [Assigment_01] = @Assigment_01, [Assigment_02] = @Assigment_02, [Exam] = @Exam WHERE [ID] = @original_ID AND [UnitCode] = @original_UnitCode AND [StudentID] = @original_StudentID AND (([SemesterID] = @original_SemesterID) OR ([SemesterID] IS NULL AND @original_SemesterID IS NULL)) AND (([Year] = @original_Year) OR ([Year] IS NULL AND @original_Year IS NULL)) AND (([Assigment_01] = @original_Assigment_01) OR ([Assigment_01] IS NULL AND @original_Assigment_01 IS NULL)) AND (([Assigment_02] = @original_Assigment_02) OR ([Assigment_02] IS NULL AND @original_Assigment_02 IS NULL)) AND (([Exam] = @original_Exam) OR ([Exam] IS NULL AND @original_Exam IS NULL))">
+            <DeleteParameters>
+                <asp:Parameter Name="original_ID" Type="Int32" />
+                <asp:Parameter Name="original_UnitCode" Type="String" />
+                <asp:Parameter Name="original_StudentID" Type="String" />
+                <asp:Parameter Name="original_SemesterID" Type="String" />
+                <asp:Parameter Name="original_Year" Type="String" />
+                <asp:Parameter Name="original_Assigment_01" Type="Int32" />
+                <asp:Parameter Name="original_Assigment_02" Type="Int32" />
+                <asp:Parameter Name="original_Exam" Type="Int32" />
+            </DeleteParameters>
+            <InsertParameters>
+                <asp:ControlParameter ControlID="unitCode" Name="UnitCode" PropertyName="Text" Type="String" />
+                <asp:ControlParameter ControlID="studentID" Name="StudentID" PropertyName="Text" Type="String" />
+                <asp:ControlParameter ControlID="semester" Name="SemesterID" PropertyName="Text" Type="String" />
+                <asp:ControlParameter ControlID="year" Name="Year" PropertyName="Text" Type="String" />
+                <asp:ControlParameter ControlID="a1" Name="Assigment_01" PropertyName="Text" Type="Int32" />
+                <asp:ControlParameter ControlID="a2" Name="Assigment_02" PropertyName="Text" Type="Int32" />
+                <asp:ControlParameter ControlID="exam" Name="Exam" PropertyName="Text" Type="Int32" />
+            </InsertParameters>
+            <UpdateParameters>
+                <asp:Parameter Name="UnitCode" Type="String" />
+                <asp:Parameter Name="StudentID" Type="String" />
+                <asp:Parameter Name="SemesterID" Type="String" />
+                <asp:Parameter Name="Year" Type="String" />
+                <asp:Parameter Name="Assigment_01" Type="Int32" />
+                <asp:Parameter Name="Assigment_02" Type="Int32" />
+                <asp:Parameter Name="Exam" Type="Int32" />
+                <asp:Parameter Name="original_ID" Type="Int32" />
+                <asp:Parameter Name="original_UnitCode" Type="String" />
+                <asp:Parameter Name="original_StudentID" Type="String" />
+                <asp:Parameter Name="original_SemesterID" Type="String" />
+                <asp:Parameter Name="original_Year" Type="String" />
+                <asp:Parameter Name="original_Assigment_01" Type="Int32" />
+                <asp:Parameter Name="original_Assigment_02" Type="Int32" />
+                <asp:Parameter Name="original_Exam" Type="Int32" />
+            </UpdateParameters>
+        </asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ exce/exelUpload.aspx.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace Final_Assignment.ManageResults
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Manager"] == null)// if admin session is null (user not logged in properly from login page)
+            {
+                Response.Redirect("/Account/Login.aspx");
+            }
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            String UnitCode;
+            String StudentID;
+            String SemesterID;
+            String Year;
+            String Assignment_01;
+            String Assignment_02;
+            String Exam;
+            String regexUnitCode = @"^[a-zA-Z]{3}[0-9]{4}$";
+            String regexStudentID = @"^[0-9]{8}$";
+            String regexYear = @"^([0-9]{4})$";
+            int rowCounter = 0;
+            String errorString = "<br>Error Summary, ( Please check the exel file ) " + "<br>Following rows did not add to the data base <br><br>";
+            bool containErrors = false;
+            if(ufExel.HasFile)
+            {
+                String fileType = Path.GetExtension(ufExel.FileName);
+                if (fileType.ToLower() == ".xlsx")
+                {
+                    string path = Path.GetFileName(ufExel.FileName);
+                    path = path.Replace(" ", "");
+                    ufExel.SaveAs(Server.MapPath("~/Data/") + path);
+                    String ExcelPath = Server.MapPath("~/Data/") + path;
+                    OleDbConnection mycon = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + ExcelPath + "; Extended Properties=Excel 8.0; Persist Security Info = False");
+                    mycon.Open();
+                    OleDbCommand cmd = new OleDbCommand("select * from [Sheet1$]", mycon);
+                    OleDbDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        try
+                        {
+                            rowCounter++;
+                            containErrors = false;
+                            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LoginConnectionString"].ConnectionString);
+                            UnitCode = dr[0].ToString().Trim();
+                            if (!Regex.IsMatch(UnitCode, regexUnitCode))
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Invalid Unit Code format [aaa1111]" + "<br>";
+                                containErrors = true;
+                            }
+                            StudentID = dr[1].ToString().Trim();
+                            if (!Regex.IsMatch(StudentID, regexStudentID))
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Invalid Student ID format [11111111]" + "<br>";
+                                containErrors = true;
+                            }
+                            SemesterID = "0" + dr[2].ToString().Trim();
+                            if (!(SemesterID == "01" || SemesterID == "02"))
+                            {
+                                errorString += "* Row number : " + rowCounter + " " + SemesterID + " - Invalid Semester" + "<br>";
+                                containErrors = true;
+                            }
+                            Year = dr[3].ToString().Trim();
+                            if (!Regex.IsMatch(Year, regexYear))
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Invalid Year format [yyyy]" + "<br>";
+                                containErrors = true;
+                            }
+                            Assignment_01 = dr[4].ToString().Trim();
+                            if (!(Convert.ToInt32(Assignment_01) >= 0 && Convert.ToInt32(Assignment_01) <= 20))
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Assignment 01 marks out of range" + "<br>";
+                                containErrors = true;
+                            }
+                            Assignment_02 = dr[5].ToString().Trim();
+                            if (!(Convert.ToInt32(Assignment_02) >= 0 && Convert.ToInt32(Assignment_02) <= 20))
+                            {
+                                errorString += "*  Row number : " + rowCounter + " - Assignment 02 marks out of range" + "<br>";
+                                containErrors = true;
+                            }
+                            Exam = dr[6].ToString().Trim();
+                            if (!(Convert.ToInt32(Exam) >= 0 && Convert.ToInt32(Exam) <= 60))
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Exam marks out of range" + "<br>";
+                                containErrors = true;
+                            }
+
+                            unitCode.Text = UnitCode;
+                            unitCode.Visible = false;
+                            con.Open();
+                            string checkUnit = "select UnitCode from UnitData where UnitCode='" + unitCode.Text + "'";//to check unit exists in the database
+                            SqlCommand comUnit = new SqlCommand(checkUnit, con);
+                            if (comUnit.ExecuteScalar() == null)
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Unit Code does not exist in the data base ( " + unitCode.Text + " )<br>";
+                                containErrors = true;
+                            }
+                            con.Close();
+
+                            studentID.Text = StudentID;
+                            studentID.Visible = false;
+                            con.Open();
+                            string checkStundet = "select StudentID from StudentData where StudentID='" + studentID.Text + "'";// to check student id registered inside the student data table
+                            SqlCommand comStudent = new SqlCommand(checkStundet, con);
+                            if (comStudent.ExecuteScalar() == null)
+                            {
+                                errorString += "* Row number : " + rowCounter + " - Student ID does not exist in the data base ( " + studentID.Text + " )<br>";
+                                containErrors = true;
+                            }
+                            con.Close();
+
+                            semester.Text = SemesterID;
+                            semester.Visible = false;
+
+                            year.Text = Year;
+                            year.Visible = false;
+
+                            a1.Text = Assignment_01;
+                            a1.Visible = false;
+
+                            a2.Text = Assignment_02;
+                            a2.Visible = false;
+
+                            exam.Text = Exam;
+                            exam.Visible = false;
+
+                            if (containErrors)
+                            {
+                                lblError.Visible = true;
+                            }
+                            else
+                            {
+                                dsInsertResult.Insert();
+                            }
+                        }
+                        catch (System.Exception error)// will catch any exception thrown because of invalid excel table format
+                            {
+                            lblError.Text = " Invalid excel table format [ Must have 7 columns ]";
+                            lblStatus.Visible = true;
+                            mycon.Close();
+                            return;
+                            }
+                        }
+                    mycon.Close();
+                    lblError.Text = errorString;
+                    lblStatus.Visible = true;
+                    File.Delete(ExcelPath);// delete the exel file after reading data
+                }
+                else
+                {
+                    lblFileStatus.Text = "Invalid file type, Please insert a .xslx type file";
+                    lblFileStatus.ForeColor = System.Drawing.Color.Red;
+                    lblFileStatus.Visible = true;
+                }
+            }
+            else
+            {
+                lblFileStatus.Text = "Please insert a file to upload";
+                lblFileStatus.ForeColor = System.Drawing.Color.Red;
+                lblFileStatus.Visible = true;
+            }
+        }
+        protected void lbLogout_Click(object sender, EventArgs e)
+        {
+            Session["Manager"] = null;
+            Response.Redirect("/Account/Login.aspx");
+        }
+
+        protected void lbAddResults_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/ManageResults/addResults.aspx");
+        }
+
+        protected void lbHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/ManageResults/ManageResults.aspx");
+        }
+    }
+}
